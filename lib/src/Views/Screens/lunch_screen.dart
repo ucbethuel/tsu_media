e7cda0618913a68314def2c51d4c55031384d4fb
ucbethuel <bethuelorji@gmail.com>
2024-04-20 09:06:27 +0100

    modify my lunch_screen view

--- lib/src/Views/Screens/lunch_screen.dart
+++ lib/src/Views/Screens/lunch_screen.dart
@@ -23,51 +23,13 @@ class _LunchScreenState extends State<LunchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //Body of the lunch Screen, Everything wrap in the Column widget
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          //logo
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
-                child: Image.asset(
-                  "assets/images/logo.png",
-                  height: 85.0,
-                  width: 85.0,
-                ),
-              ),
-            ],
-          ),
-          //Developer quick profile
-          // const Row(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   crossAxisAlignment: CrossAxisAlignment.end,
-          //   children: [
-          //     CircleAvatar(
-          //       backgroundColor: Colors.grey,
-          //     ),
-          //     VerticalDivider(
-          //       color: Colors.black,
-          //       thickness: 100,
-          //     ),
-
-          // Column(
-          //   children: [
-          //     Text("Developed by"),
-          //     Text(
-          //       "ucbethuel",
-          //       style: TextStyle(letterSpacing: 5.0),
-          //     ),
-          //   ],
-          // ),
-          //   ],
-          // )
-        ],
+        //Body of the lunch Screen, Everything wrap in the Column widget
+        body: Center(
+      child: Image.asset(
+        "assets/images/logo.png",
+        height: 100.0,
+        width: 100.0,
       ),
-    );
+    ));
   }
 }
